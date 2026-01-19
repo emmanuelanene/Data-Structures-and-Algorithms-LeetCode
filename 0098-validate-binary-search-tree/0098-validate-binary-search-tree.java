9c66eb6effa4c11cf70993dda877e86b75c56276
Emmanuel Anene <86776560+emmanuelanene@users.noreply.github.com>
2026-01-19 23:32:44 +0100

    Time: 3 ms (3.39%), Space: 46.8 MB (5.3%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        Queue<Object[]> queue = new LinkedList<>();
+        queue.offer(new Object[]{root, Long.MIN_VALUE, Long.MAX_VALUE});
+
+        while (!queue.isEmpty()) {
+            Object[] current = queue.poll();
+            TreeNode node = (TreeNode) current[0];
+            long left = (long) current[1];
+            long right = (long) current[2];
+
+            if (!(left < node.val && node.val < right)) {
+                return false;
+            }
+
+            if (node.left != null) {
+                queue.offer(new Object[]{node.left, left, (long) node.val});
+            }
+            if (node.right != null) {
+                queue.offer(new Object[]{node.right, (long) node.val, right});
+            }
+        }
+
+        return true;
+    }
+}
